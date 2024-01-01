ee7b11230133668cea5fd7a3a8d898e3f83e362a
seoyuncho <seoyuncho@korea.ac.kr>
2024-01-01 20:16:23 +0900

    feat: 연락처 전화, 문자 기능 추가

--- app/src/main/java/com/example/tab_layout/ContactDetailFragment.java
+++ app/src/main/java/com/example/tab_layout/ContactDetailFragment.java
@@ -3,13 +3,14 @@ package com.example.tab_layout;
 import android.content.Context;
 import android.os.Bundle;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
 import android.database.sqlite.SQLiteDatabase;
-import com.example.tab_layout.DBHelper;
 
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
@@ -57,6 +58,10 @@ public class ContactDetailFragment extends Fragment {
         // 이름 및 전화번호 띄우기
         TextView nameTextView = view.findViewById(R.id.detailNameTextView);
         TextView phoneNumTextView = view.findViewById(R.id.detailPhoneNumTextView);
+        Button deleteButton = view.findViewById(R.id.deleteContactButton);
+        Button callButton = view.findViewById(R.id.callButton);
+        Button messageButton = view.findViewById(R.id.textButton);
+
         Bundle args = getArguments();
         if (args != null) {
             String name = args.getString("name", "");
@@ -67,7 +72,6 @@ public class ContactDetailFragment extends Fragment {
         }
 
         //삭제 버튼 띄우기
-        Button deleteButton = view.findViewById(R.id.deleteContactButton);
         deleteButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -105,6 +109,31 @@ public class ContactDetailFragment extends Fragment {
                     // 트랜잭션을 커밋하여 변경사항을 적용
                     fragmentTransaction.commit();
                 }
+            }
+        });
+
+        callButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // 전화 버튼 클릭 시 전화 앱을 열도록 함
+                String phoneNum = args.getString("phoneNum", "");
+                Intent dialIntent = new Intent(Intent.ACTION_DIAL, Uri.parse("tel:" + phoneNum));
+                startActivity(dialIntent);
+            }
+        });
+
+        // 문자 보내기 버튼 클릭 시 동작
+        messageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // 문자 보내기 동작 구현
+                String phoneNum = args.getString("phoneNum", "");
+                Intent smsIntent = new Intent(Intent.ACTION_SEND);
+                smsIntent.setType("text/plain");
+                smsIntent.putExtra(Intent.EXTRA_TEXT, "");
+                smsIntent.putExtra("address", phoneNum);
+
+                startActivity(smsIntent);
             }
         });
 
